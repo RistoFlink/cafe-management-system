376d5be1a66610371d84cc7da799b9ac9cb918e5
RistoFlink <kenssi@gmail.com>
2023-09-17 20:55:11 +0300

    Add methods related to JWT

---
+++ src/main/java/dev/ristoflink/cafemanagementsystem/jwt/JwtUtil.java
@@ -0,0 +1,61 @@
+package dev.ristoflink.cafemanagementsystem.jwt;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Service
+public class JwtUtil {
+    //using this to try and hide the secret key
+    Dotenv dotenv = Dotenv.load();
+    private final String secretKey = dotenv.get("SECRET");
+    private final String secret = secretKey;
+
+    public String extractUsername(String token) {
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    public Date extractExpirationDate(String token) {
+        return extractClaims(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaims(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    public Claims extractAllClaims(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+    private boolean hasTokenExpired(String token) {
+        return extractExpirationDate(token).before(new Date());
+    }
+
+    public String generateToken(String username, String role){
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("role", role);
+        return createToken(claims, username);
+    }
+    private String createToken(Map<String, Object> claims, String username){
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(username)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10))
+                .signWith(SignatureAlgorithm.HS256, secret).compact();
+    }
+
+    public boolean validateToken(String token, UserDetails userDetails){
+        final String userName = extractUsername(token);
+        return (userName.equals(userDetails.getUsername()) && !hasTokenExpired(token));
+    }
+}

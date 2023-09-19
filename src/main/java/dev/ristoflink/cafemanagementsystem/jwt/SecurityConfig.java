823f89c3fa556cf3450da8d5642b5911d8edd311
RistoFlink <kenssi@gmail.com>
2023-09-19 15:50:44 +0300

    Try to replace deprecated parts in SecurityConfig

--- src/main/java/dev/ristoflink/cafemanagementsystem/jwt/SecurityConfig.java
+++ src/main/java/dev/ristoflink/cafemanagementsystem/jwt/SecurityConfig.java
@@ -5,12 +5,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -22,9 +26,20 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests((auths) -> auths.anyRequest().authenticated()).httpBasic(withDefaults());
+        http
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((auths) -> auths
+                        .requestMatchers("/user/**").permitAll()
+                        .anyRequest().authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(withDefaults());
+
         return http.build();
     }
+
+    // maybe insert this back later   .authorizeHttpRequests((auths) -> auths.anyRequest().authenticated()).httpBasic(withDefaults());
+    //not sure yet..
 
     @Bean
     public PasswordEncoder passwordEncoder(){
